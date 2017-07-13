2c0300f2e6c6fc60a2a9e20f3dbf7ac8e3b6b1e0
Cerem Cem ASLAN <cem@aktos.io>
2017-07-14 00:37:13 +0300

    make-ssh and make-backup works basically

---
+++ common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set_dir () { DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; }
+safe_source () { source $1; set_dir; }
+set_dir
+
+safe_source $DIR/config.sh
+
+[ $NODE_USER ] || NODE_USER="aea"
+[ $KEY_FILE ] || KEY_FILE="$HOME/.ssh/id_rsa"
+[ $MOUNT_DIR ] || MOUNT_DIR=$(mktemp -d)
+[ $NODE_IP ] || die "Node IP is required!"
+[ $NODE_PORT ] || NODE_PORT=22
+
+echo_green "Target: $NODE_USER@$NODE_IP:$NODE_PORT"
+if [ $RENDEZVOUS_HOST ]; then
+    [ $RENDEZVOUS_USER ] || die "Rendezvous username is required"
+    [ $RENDEZVOUS_PORT ] || RENDEZVOUS_PORT=443
+    [ $NODE_RENDEZVOUS_PORT ] || die "Target node's sshd port on rendezvous server is required"
+    echo_green "Using rendezvous server: $RENDEZVOUS_USER@$RENDEZVOUS_HOST:$RENDEZVOUS_PORT -> $NODE_RENDEZVOUS_PORT"
+fi
