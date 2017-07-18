677357fa35f5409e722deaa7191c39b8b35dab16
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-07-18 04:10:59 +0300

    Update config-template.sh

--- config-template.sh
+++ config-template.sh
@@ -1,15 +1,17 @@
 # Direct (on LAN) connection settings:
 # ------------------------------------
-NODE_IP="10.0.8.2"
-#NODE_IP="10.0.10.140"
-
+NODE_IP="xx.yy.zz.tt"
 #NODE_PORT=22
 #NODE_USER="aea"
-#KEY_FILE="/$HOME/.ssh/id_rsa"
+#KEY_FILE="$HOME/.ssh/id_rsa"
 #MOUNT_DIR=$(mktemp -d)
 
 
 # Proxy connection settings
 # ------------------------------------
-SSH_SOCKET_FILE="/tmp/ssh-mobmac2@aktos.io:443.sock"
-NODE_RENDEZVOUS_PORT=7103
+# create master socket first: 
+#
+#     ssh you@example.com -p 1234 -M -S /path/to/ssh-master-you@example.com:1234.socket
+#
+SSH_SOCKET_FILE="/path/to/ssh-master-you@example.com:1234.socket"
+NODE_RENDEZVOUS_PORT=7000
