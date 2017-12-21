8c2920e2009065666421c0d55df9c690d03f2bc1
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2017-12-21 16:25:31 +0300

    Update config-template.sh

--- config-template.sh
+++ config-template.sh
@@ -1,18 +1,9 @@
-# See doc/configuration.md for details
-
-## Direct connection settings:
-## ------------------------------------
+## Direct connection settings
 NODE_IP=""
 NODE_USER=""
 NODE_PORT=22
 
-## Proxy connection settings
-## ------------------------------------
+## Proxy connection settings 
+## (see doc/configuration.md for details)
 #SSH_SOCKET_FILE="/path/to/your-socket-file"
 #NODE_RENDEZVOUS_PORT=1234
-
-## Other settings
-## ------------------------------------
-#MOUNT_DIR=/path/to/your-mount-dir
-#KEY_FILE="/path/to/your/id_rsa"
-
