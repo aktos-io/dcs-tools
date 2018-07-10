42fa2be14d00301e9ca37743cbf02f34cba0d153
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2018-07-10 19:16:43 +0300

    Update config-template.sh

--- config-template.sh
+++ config-template.sh
@@ -6,4 +6,4 @@ NODE_ADDR="direct-ip-or-addr[:port=22]"
 ## (see doc/configuration.md for details)
 PROXY_USER="otheruser"
 PROXY_ADDR="server-ip-or-addr[:port=22]"
-NODE_PROXY_PORT=1234 # SSHD port of the target on proxy server
+NODE_PROXY_PORT=1234
