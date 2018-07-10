4532c517220e6e9088612ab95c8e07f589945ce9
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2018-07-10 19:14:24 +0300

    Update config-template.sh

--- config-template.sh
+++ config-template.sh
@@ -1,9 +1,9 @@
 ## Direct connection settings
-NODE_IP=""
-NODE_USER=""
-NODE_PORT=22
+NODE_USER="myuser"
+NODE_ADDR="direct-ip-or-addr[:port=22]"
 
-## Proxy connection settings 
+## Proxy connection settings
 ## (see doc/configuration.md for details)
-#SSH_SOCKET_FILE="/path/to/your-socket-file"
-#NODE_RENDEZVOUS_PORT=1234
+PROXY_USER="otheruser"
+PROXY_ADDR="server-ip-or-addr[:port=22]"
+NODE_PROXY_PORT=1234 # SSHD port of the target on proxy server
